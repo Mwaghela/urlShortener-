8664c7412412b8050130c8e9323888a081605dc6
Manish Waghela <manish.b.waghela@gmail.com>
2018-10-03 01:06:35 +0530

    Rest Api created with index.jsp Mysql connectivity added

---
+++ restApi/src/main/java/com/restApi/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.restApi.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import gvjava.org.json.JSONObject;
+
+@Controller
+public class BaseController {
+	
+	
+}
---
+++ restApi/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Url Shortener Module Started!!!</h2>
+</body>
+</html>
